3ddde0f9f18a17fb0cbe03dd901f6280e060b9fe
xiajunli <309644739@qq.com>
2017-05-02 16:46:40 +0800

    添加构造器注入，以及junitTest

--- src/main/java/com/spring/Himma.java
+++ src/main/java/com/spring/Himma.java
@@ -1,19 +1,26 @@
 package com.spring;
 
 public class Himma implements People {
-
-	private int brick = 100;
+	//默认一天搬100块砖
+	private int brickNum = 100;
+	//默认填5个坑
+	private int pitNum = 5;
 	
 	public Himma(){
 		
 	}
 	
-	public Himma(int brick){
-		this.brick = brick;
+	public Himma(int brickNum){
+		this.brickNum = brickNum;
+	}
+	
+	public Himma(int brickNum, int pitNum){
+		this.brickNum = brickNum;
+		this.pitNum= pitNum;
 	}
 	
 	public void doWorking() {
-		System.out.println("搬了"+brick+"块砖");
+		System.out.println("今天搬了"+brickNum+"块砖,还填了"+pitNum+"个坑");
 	}
 
 }
--- src/main/java/com/spring/Main.java
+++ src/main/java/com/spring/Main.java
@@ -7,7 +7,13 @@ public class Main {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Spring-WiringBean.xml");
-		People xiaoming = (People)applicationContext.getBean("xiaoming");
+		People xiaoming = (People)applicationContext.getBean("xiaoMing");
 		xiaoming.doWorking();
+		
+		People xiaohong = (People)applicationContext.getBean("xiaoHong");
+		xiaohong.doWorking();
+		
+		People xiaogang = (People)applicationContext.getBean("xiaoGang");
+		xiaogang.doWorking();
 	}
 }
